85d0625c98163de2fc2436f0dcf342df9afb27a9
Evan Dorn <evan@lrdesign.com>
2011-03-21 21:39:33 -0700

    added jupiter

--- main.lua
+++ main.lua
@@ -3,7 +3,9 @@ ndraws = 0
 CENTER = { x = 200, y = 300 }
 MARS_RADIUS = 150
 EARTH_RADIUS = 100
-SPEED = 3
+JUPITER_RADIUS = 250
+
+SPEED = 4
 
 function love.draw()
     love.graphics.setColor(255,255,255)
@@ -20,6 +22,12 @@ function love.draw()
     x, y = orbit_coords(ndraws, SPEED, EARTH_RADIUS, CENTER)
     love.graphics.setColor(50, 60, 200) -- Earth
     love.graphics.circle("fill", x, y, 10, 50)
+
+    x, y = orbit_coords(ndraws, SPEED, JUPITER_RADIUS, CENTER)
+    love.graphics.setColor(250, 70, 70) -- Jupiter
+    love.graphics.circle("fill", x, y, 30, 50)
+    love.graphics.setColor(150, 20, 10) -- Jupiter
+    love.graphics.circle("fill", x+6, y+4, 10, 20)
 
     ndraws = ndraws + 1
 end
