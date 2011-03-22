7543f12b0c39edd2e640f94d1bb1c7e41cdc666b
Evan Dorn <evan@lrdesign.com>
2011-03-21 21:33:09 -0700

    added earth

--- main.lua
+++ main.lua
@@ -1,13 +1,13 @@
 ndraws = 0
 
-CENTER = { x = 150, y = 300 }
+CENTER = { x = 200, y = 300 }
 MARS_RADIUS = 150
 EARTH_RADIUS = 100
 SPEED = 3
 
 function love.draw()
     love.graphics.setColor(255,255,255)
-    love.graphics.print("Hello Kiri!", 400, 300)
+    love.graphics.print("I Love Kiri!", 500, 300)
     love.graphics.print(string.format("%d draws.", ndraws), 400, 350)
     love.graphics.setColor(200, 200, 0)
     love.graphics.circle("fill", CENTER.x, CENTER.y, 50, 50)
@@ -18,7 +18,7 @@ function love.draw()
     love.graphics.circle("fill", x, y, 10, 50)
 
     x, y = orbit_coords(ndraws, SPEED, EARTH_RADIUS, CENTER)
-    love.graphics.setColor(50, 60, 200) -- Mars
+    love.graphics.setColor(50, 60, 200) -- Earth
     love.graphics.circle("fill", x, y, 10, 50)
 
     ndraws = ndraws + 1
@@ -27,4 +27,5 @@ end
 function orbit_coords(ndraws, speed, radius, center)
   x = (math.sin(ndraws * speed / radius) * radius) + center.x
   y = (math.cos(ndraws * speed / radius) * radius) + center.y
+  return x, y
 end
