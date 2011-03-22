ee4fe7c1cf75ec98532015e39058d3a6d44adae7
Kiri Wagstaff <wkiri@wkiri.com>
2011-03-21 21:19:45 -0700

    We have a SUN and a MARS!

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+ndraws = 0
+
 function love.draw()
+    love.graphics.setColor(255,255,255)
     love.graphics.print("Hello Kiri!", 400, 300)
+    love.graphics.print(string.format("%d draws.", ndraws), 400, 350)
+    love.graphics.setColor(200, 200, 0)
+    love.graphics.circle("fill", 100, 300, 50, 50)
+
+    love.graphics.setColor(200, 0, 0) -- Mars 
+    love.graphics.circle("fill", ndraws/10, ndraws/10, 10, 50)
+
+    ndraws = ndraws + 1
 end
