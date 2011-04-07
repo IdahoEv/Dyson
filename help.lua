3b947c7f6512a42a0d113d7f1f33c6670d6be467
Kiri Wagstaff <wkiri@wkiri.com>
2011-04-06 22:03:07 -0700

    Noticed we had two drawHelp() functions defined, and removed the one in main.lua in favor of keeping it defined in help.lua instead.

--- help.lua
+++ help.lua
@@ -2,9 +2,16 @@ function drawHelp()
   textLine{str = "Key Commands:", x = HELP_MARGIN, y = TOP_MARGIN }
   textLine{str = "right/left: change speed"}
   textLine{str = "up/down: zoom"}
+  textLine{str = "shift+0: recenter to star (Sol)"}
+  textLine{str = "shift+<n>: recenter to planet n"}
   textLine{str = 'f: toggle fullscreen'}
   textLine{str = 'p: toggle enlarge planets'}
   textLine{str = 'o: toggle show orbits'}
   textLine{str = 'r: toggle planet reticles'}
+  textLine{str = '<space>: pause'}
   textLine{str = 'q: quit'}
+  textLine{str = ''}
+  textLine{str = 'Left-click to recenter on a spob'}
+  textLine{str = 'Right-click to recenter at origin'}
 end
+
--- main.lua
+++ main.lua
@@ -125,23 +125,6 @@ function love.draw()
   ndraws = ndraws + 1
 end
 
-function drawHelp()
-  textLine{str = "Key Commands:", x = HELP_MARGIN, y = TOP_MARGIN }
-  textLine{str = "right/left: change speed"}
-  textLine{str = "up/down: zoom"}
-  textLine{str = "shift+0: recenter to star (Sol)"}
-  textLine{str = "shift+<n>: recenter to planet n"}
-  textLine{str = 'f: toggle fullscreen'}
-  textLine{str = 'p: toggle enlarge planets'}
-  textLine{str = 'o: toggle show orbits'}
-  textLine{str = 'r: toggle planet reticles'}
-  textLine{str = '<space>: pause'}
-  textLine{str = 'q: quit'}
-  textLine{str = ''}
-  textLine{str = 'Left-click to recenter on a spob'}
-  textLine{str = 'Right-click to recenter at origin'}
-end
-
 -- Naive first version just iterates the stars to see if they are within a delta of onscreen,
 -- and adds their satellites if those satellites' orbits aren't too small
 function findVisibleSpobs(stars_to_check)
