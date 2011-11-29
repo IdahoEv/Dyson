a4aa5ab0354b61b5da0076c86722c9e5dc4e14f2
Kiri Wagstaff <wkiri@wkiri.com>
2011-11-28 21:27:15 -0800

    Updated construct and paintable_construct to use matrices for space coords.

--- paintable_construct.lua
+++ paintable_construct.lua
@@ -1,5 +1,6 @@
 require "middleclass" -- definition of useful class construction methods
 require "paintable"
+local matrix = require 'lua-matrix/lua/matrix'
 
 PaintableConstruct = Paintable:subclass('PaintableConstruct')
 
@@ -9,22 +10,28 @@ function PaintableConstruct:initialize(spob, color)
 end
 
 function PaintableConstruct:draw()
-  segments = self.spob.segments
+  local segments = self.spob.segments
 
   love.graphics.setColor(self.color.R, self.color.G, self.color.B)
 
-  construct_location = self.spob:getLocation()
+  local construct_location = self.spob:getLocation()
   for seg_i, segment in pairs(self.spob.segments) do
     for face_i, face in pairs(segment.faces) do
       polygon = {}
       for _, point in pairs(face) do
-	sin_theta = math.sin(self.spob.rotation_angle)
-	cos_theta = math.cos(self.spob.rotation_angle)
-	local rot_p = { x = point.x * cos_theta - point.y * sin_theta,
-			y = point.x * sin_theta + point.y * cos_theta }
-	local p = { x = rot_p.x + construct_location.x,
-		    y = rot_p.y + construct_location.y }
-        screen_x, screen_y = scale:screenCoords(p)
+	local theta = self.spob.rotation_angle
+	local sin_theta = math.sin(theta)
+	local cos_theta = math.cos(theta)
+	--local rot_p = { x = point.x * cos_theta - point.y * sin_theta,
+	--		y = point.x * sin_theta + point.y * cos_theta }
+	-- This is a 3D rotation matrix around the z axis
+	local rot_p = matrix{{cos_theta,-sin_theta,0},
+			     {sin_theta, cos_theta,0},
+			     {        0,         0,1}} * point    
+	--local p = { x = rot_p.x + construct_location.x,
+	--	    y = rot_p.y + construct_location.y }
+	local p = rot_p + construct_location -- yummy, I'm a matrix
+        local screen_x, screen_y = scale:screenCoords(p)
         table.insert(polygon, screen_x)
         table.insert(polygon, screen_y)
       end
